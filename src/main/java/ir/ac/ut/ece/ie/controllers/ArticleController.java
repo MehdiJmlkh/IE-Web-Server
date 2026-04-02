67fed26dacee6938ce040985a4a0bef208610a41
Mehdi <mehdijmlkh1382@gmail.com>
2026-04-02 23:05:57 +0330

    refactor: add a method for filtering the articles to the article controller

--- src/main/java/ir/ac/ut/ece/ie/controllers/ArticleController.java
+++ src/main/java/ir/ac/ut/ece/ie/controllers/ArticleController.java
@@ -1,14 +1,23 @@
 package ir.ac.ut.ece.ie.controllers;
 
+import ir.ac.ut.ece.ie.http.HttpResponse;
 import ir.ac.ut.ece.ie.services.Article;
 import ir.ac.ut.ece.ie.services.ArticleService;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 
 public class ArticleController {
-    public static void addArticle(Map<String, String> payload) {
+    public static void addArticle(Map<String, String> payload, HttpResponse httpResponse) throws IOException, ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
         Article article = new Article(payload.get("title"), payload.get("abstract"), payload.get("body"), 2000);
         ArticleService.getInstance().addArticle(article);
+        httpResponse.writePage("ShowArticles", null);
     }
 
+
+    public static void filterArticles(Map<String, String> payload, HttpResponse httpResponse) throws IOException {
+        ArticleService.getInstance().setFilter(payload.get("search"));
+        httpResponse.sendNoContentResponse();
+    }
 }
--- src/main/java/ir/ac/ut/ece/ie/controllers/Controller.java
+++ src/main/java/ir/ac/ut/ece/ie/controllers/Controller.java
@@ -11,13 +11,10 @@ public class Controller {
     public static void handleAction(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
         String command = httpRequest.getPath();
         if (command.equals("filter-articles")) {
-            ArticleService.getInstance().setFilter(httpRequest.getPayload().get("search"));
-            httpResponse.writePage("ShowArticles", null);
+            ArticleController.filterArticles(httpRequest.getPayload(), httpResponse);
         }
         else {
-            System.out.println(httpRequest.getPayload());
-            ArticleController.addArticle(httpRequest.getPayload());
-            httpResponse.sendNoContentResponse();
+            ArticleController.addArticle(httpRequest.getPayload(), httpResponse);
         }
     }
 }
