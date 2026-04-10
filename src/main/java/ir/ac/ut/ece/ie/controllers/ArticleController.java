acf031545b6101b7a2f7a37b011ce6958f719eef
Mehdi <mehdijmlkh1382@gmail.com>
2026-04-10 13:58:41 +0330

    refactor: add ArticleService

--- src/main/java/ir/ac/ut/ece/ie/controllers/ArticleController.java
+++ src/main/java/ir/ac/ut/ece/ie/controllers/ArticleController.java
@@ -3,23 +3,20 @@ package ir.ac.ut.ece.ie.controllers;
 import ir.ac.ut.ece.ie.dtos.AddArticleRequest;
 import ir.ac.ut.ece.ie.dtos.FilterArticlesRequest;
 import ir.ac.ut.ece.ie.http.HttpResponse;
-import ir.ac.ut.ece.ie.entities.Article;
-import ir.ac.ut.ece.ie.repositories.ArticleRepository;
+import ir.ac.ut.ece.ie.services.ArticleService;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
 public class ArticleController {
-    public static HttpResponse addArticle(AddArticleRequest request) throws IOException, ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
-        Article article = new Article(request.getTitle(), request.getAbs(), request.getBody(), 2000);
-        ArticleRepository.getInstance().addArticle(article);
 
+    public static HttpResponse addArticle(AddArticleRequest request) throws IOException, ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+        ArticleService.addArticle(request);
         return HttpResponse.writePage("ShowArticles", null);
     }
 
-
     public static HttpResponse filterArticles(FilterArticlesRequest request) throws IOException {
-        ArticleRepository.getInstance().setFilter(request.getSearchInput());
+        ArticleService.filterArticles(request);
         return HttpResponse.noContent();
     }
 }
---
+++ src/main/java/ir/ac/ut/ece/ie/services/ArticleService.java
@@ -0,0 +1,17 @@
+package ir.ac.ut.ece.ie.services;
+
+import ir.ac.ut.ece.ie.dtos.AddArticleRequest;
+import ir.ac.ut.ece.ie.dtos.FilterArticlesRequest;
+import ir.ac.ut.ece.ie.entities.Article;
+import ir.ac.ut.ece.ie.repositories.ArticleRepository;
+
+public class ArticleService {
+    public static void addArticle(AddArticleRequest request) {
+        Article article = new Article(request.getTitle(), request.getAbs(), request.getBody(), 2000);
+        ArticleRepository.getInstance().addArticle(article);
+    }
+
+    public static void filterArticles(FilterArticlesRequest request) {
+        ArticleRepository.getInstance().setFilter(request.getSearchInput());
+    }
+}
